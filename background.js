d51a6a4b3ded7e324559643972fdc31261a8ccd7
Epicrider <omarepic1@gmail.com>
2020-10-03 12:09:45 -0700

    Add Basic Popup functionality on temporary site developer.chrome.com

--- background.js
+++ background.js
@@ -2,4 +2,16 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
         console.log("The color is green.");
     });
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'developer.chrome.com' },
+            })
+            ],
+            // ShowPageAction() corresponds to "page_action" in manifest.json
+            // Will have colored icons when user goes to an article that is politically charged
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
